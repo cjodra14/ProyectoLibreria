43b2a9f5c828df8fd0a6cc630c35217b35b02a51
Christian Jodra <c.jodra14@gmail.com>
2021-12-16 19:22:31 +0100

    Genera TXTs

--- src/com/carjotu/swing/VentanaSwingVentas.java
+++ src/com/carjotu/swing/VentanaSwingVentas.java
@@ -7,7 +7,14 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.sql.SQLException;
+import java.text.DecimalFormat;
 import java.util.Iterator;
 import java.util.Vector;
 
@@ -26,7 +33,6 @@ import javax.swing.table.DefaultTableModel;
 import com.carjotu.controler.ControladorAutor;
 import com.carjotu.controler.ControladorLibro;
 import com.carjotu.controler.ControladorVenta;
-import com.carjotu.dao.DAOAutor;
 import com.carjotu.dao.DAOLibro;
 import com.carjotu.dao.DAOVenta;
 import com.carjotu.dao.DAOVenta_libro;
@@ -41,16 +47,12 @@ public class VentanaSwingVentas {
 	
 	private JLabel lblListadoDeVenta;
 	private JTable tableVentas;
-	private JButton btnBotonEditar;
-	private JButton btnBotonEliminar;
+	private JButton btnBotonGenTXT;
+	private JButton btnBotonGenXML;
 	private JButton btnBotonAniadir;
-	private JLabel lblCodigoDeAutor;
 	private JTextField textFieldCodigoAutor;
-	private JLabel lblNombre;
 	private JTextField textFieldNombreAutor;
-	private JLabel lblPrimerApellido;
 	private JTextField textFieldPApelAutor;
-	private JLabel lblSegundoApellido;
 	private JTextField textFieldSApelAutor;
 	private DefaultTableModel dataModel;
 	private JScrollPane scrollPane;
@@ -58,6 +60,8 @@ public class VentanaSwingVentas {
 	private int npedido=0;
 	private String fecha="";
 	private String usuario="";
+	private int filaSelecAux=-1;
+	DecimalFormat df = new DecimalFormat("#.00");
 
 	public VentanaSwingVentas(ControladorVenta controlador, JFrame marco, ControladorLibro controladorLibro) {
 		this.marco=marco;
@@ -102,36 +106,16 @@ public class VentanaSwingVentas {
 		      {
 		         int fila = tableVentas.rowAtPoint(e.getPoint());
 		         int columna = 0;
-		         if ((fila > -1) && (columna > -1))
-		        	 
+		         if ((fila > -1) && (columna > -1)) {
+		        	 filaSelecAux=fila;
 		         	npedido= Integer.valueOf((String) dataModel.getValueAt(fila, columna));
 		         	fecha=(String) dataModel.getValueAt(fila, columna+1);
-		         	usuario=(String) dataModel.getValueAt(fila, columna+2);
-		         
-		            System.out.println("Pedido nº: "+npedido+", realizado el "+fecha+" por "+usuario);
+		         	usuario=(String) dataModel.getValueAt(fila, columna+2); 
 		           
-						
-						try {
-							detalles=controlador.obtenerVentaLibro(String.valueOf(npedido));
-							double total=0;
-							Iterator<DAOVenta_libro> itVentasLibros = detalles.iterator();
-							while(itVentasLibros.hasNext()) {
-								DAOVenta_libro ventaLibro= itVentasLibros.next();
-								DAOLibro libro= controladorLibro.obtenerLibro(ventaLibro.getIsbn());
-								System.out.println("Ha pedido "+ventaLibro.getCantidad()+" unidades del libro con el ISBN "+ventaLibro.getIsbn()+" -->"+libro.getTitulo()+"  al precio de: "+libro.getPrecio());
-								total=total+(ventaLibro.getCantidad()*libro.getPrecio());	
-							}
-							System.out.println("El total del pedido es: "+total);
-						} catch (Exception exc) {
-							System.err.println("Vista: FALLO A OBTENER  VENTAS");
-							exc.printStackTrace();
-						}
-						
-		            
-		            
 		      
 		      }
-		   });
+		      
+		      }});
 		scrollPane= new JScrollPane(tableVentas);
 		scrollPane.setBounds(25, 52, 600, 275);
 		
@@ -139,62 +123,68 @@ public class VentanaSwingVentas {
 		
 	
 		
-		btnBotonEditar = new JButton("Editar Autor");
-		btnBotonEditar.setBounds(665, 98, 145, 29);
-		btnBotonEditar.addActionListener(new ActionListener() {
+		btnBotonGenTXT = new JButton("Generar TXT");
+		btnBotonGenTXT.setBounds(665, 98, 145, 29);
+		btnBotonGenTXT.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				String codAutor=textFieldCodigoAutor.getText();
-				String nombreAutor=textFieldNombreAutor.getText();
-				String pApelAutor=textFieldPApelAutor.getText();
-				String sApelAutor=textFieldSApelAutor.getText();
-				if (codAutor.equals("")||nombreAutor.equals("")) {
-					JOptionPane.showMessageDialog(null, "No puede quedar vacio ni el CODIGO DE AUTOR ni el NOMBRE ","Error",JOptionPane.INFORMATION_MESSAGE);
-				}else {
+				if (npedido>0) {
+				String userHomeFolder = System.getProperty("user.home");
+				String nombreFichero=""+npedido+".txt";
+				File textFile = new File(userHomeFolder, nombreFichero);
+				try {
+					BufferedWriter out = new BufferedWriter(new FileWriter(textFile));
+					
+					out.write("Pedido nº: "+npedido+", realizado el "+fecha+" por "+usuario+" ");
+					out.newLine();
+			           		
 					try {
-						int filasModificadas=-1;
-						//filasModificadas=controlador.modificarAutor(codAutor, nombreAutor, pApelAutor, sApelAutor);
-						if (filasModificadas>0) {
-							JOptionPane.showMessageDialog(null, "Autor modificado con exito");
-						}else {
-							JOptionPane.showMessageDialog(null, "No se ha modificado ningún autor");
+						detalles=controlador.obtenerVentaLibro(String.valueOf(npedido));
+						double total=0;
+						Iterator<DAOVenta_libro> itVentasLibros = detalles.iterator();
+						while(itVentasLibros.hasNext()) {
+							DAOVenta_libro ventaLibro= itVentasLibros.next();
+							DAOLibro libro= controladorLibro.obtenerLibro(ventaLibro.getIsbn());
+							out.write("Ha pedido "+ventaLibro.getCantidad()+" unidades del libro con el ISBN "+ventaLibro.getIsbn()+" -->"+libro.getTitulo()+"  al precio de: "+df.format(libro.getPrecio())+"€ - Subtotal:"+df.format(libro.getPrecio()*ventaLibro.getCantidad())+"€");
+							out.newLine();
+							total=total+(ventaLibro.getCantidad()*libro.getPrecio());	
 						}
-						listarVentas();
-					} catch (Exception e1) {
-						JOptionPane.showMessageDialog(null, "Error al modificar el autor");
+						out.write("El total del pedido es: "+df.format(total));
+						
+						out.close();
+						JOptionPane.showMessageDialog(null, "Fichero TXT creado con exito");
+					} catch (Exception exc) {
+						System.err.println("Vista: FALLO AL CREAR EL ARCHIVO");
+						exc.printStackTrace();
 					}
+					
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 				}
 				
-				
 			}
+				
 		});
-		panel.add(btnBotonEditar);
+		panel.add(btnBotonGenTXT);
 		
-		btnBotonEliminar = new JButton("Eliminar Autor");
-		btnBotonEliminar.addActionListener(new ActionListener() {
+		btnBotonGenXML = new JButton("Generar XML");
+		btnBotonGenXML.addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				String codAutor=textFieldCodigoAutor.getText();
-				
-				if (codAutor.equals("")) {
-					JOptionPane.showMessageDialog(null, "No puede estar vacio el CODIGO DE AUTOR a eliminar ");
-				}else {
-					try {
-						//controlador.borrarAutor(codAutor);
-						JOptionPane.showMessageDialog(null, "Autor eliminado con exito");
-						listarVentas();
-					} catch (Exception e1) {
-						JOptionPane.showMessageDialog(null, "Error al eliminar el autor");
-					}
-				}
+			
 				
 				
 			}
 		});
-		btnBotonEliminar.setBounds(665, 184, 145, 29);
-		panel.add(btnBotonEliminar);
+		btnBotonGenXML.setBounds(665, 184, 145, 29);
+		panel.add(btnBotonGenXML);
 		
 		btnBotonAniadir = new JButton("A\u00F1adir Autor");
 		btnBotonAniadir.setBounds(665, 269, 145, 29);
@@ -202,63 +192,13 @@ public class VentanaSwingVentas {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				String codAutor=textFieldCodigoAutor.getText();
-				String nombreAutor=textFieldNombreAutor.getText();
-				String pApelAutor=textFieldPApelAutor.getText();
-				String sApelAutor=textFieldSApelAutor.getText();
-				if (codAutor.equals("")||nombreAutor.equals("")) {
-					JOptionPane.showMessageDialog(null, "No puede quedar vacio ni el CODIGO DE AUTOR ni el NOMBRE ");
-				}else {
-					try {
-						//controlador.insertarAutor(codAutor, nombreAutor, pApelAutor, sApelAutor);
-						JOptionPane.showMessageDialog(null, "Autor añadido con exito");
-						listarVentas();
-					} catch (Exception e1) {
-						JOptionPane.showMessageDialog(null, "Error al añadir el autor");
-					}
-				}
+		
 				
 				
 			}
 		});
 		panel.add(btnBotonAniadir);
-		
-		lblCodigoDeAutor = new JLabel("Codigo de Autor:");
-		lblCodigoDeAutor.setBounds(15, 349, 145, 20);
-		panel.add(lblCodigoDeAutor);
-		
-		textFieldCodigoAutor = new JTextField();
-		textFieldCodigoAutor.setBounds(15, 385, 146, 26);
-		panel.add(textFieldCodigoAutor);
-		textFieldCodigoAutor.setColumns(10);
-		
-		lblNombre = new JLabel("Nombre:");
-		lblNombre.setBounds(210, 349, 69, 20);
-		panel.add(lblNombre);
-		
-		textFieldNombreAutor = new JTextField();
-		textFieldNombreAutor.setColumns(10);
-		textFieldNombreAutor.setBounds(210, 385, 146, 26);
-		panel.add(textFieldNombreAutor);
-		
-		lblPrimerApellido = new JLabel("Primer Apellido:");
-		lblPrimerApellido.setBounds(393, 349, 126, 20);
-		panel.add(lblPrimerApellido);
-		
-		textFieldPApelAutor = new JTextField();
-		textFieldPApelAutor.setColumns(10);
-		textFieldPApelAutor.setBounds(393, 385, 146, 26);
-		panel.add(textFieldPApelAutor);
-		
-		lblSegundoApellido = new JLabel("Segundo Apellido:");
-		lblSegundoApellido.setBounds(584, 349, 131, 20);
-		panel.add(lblSegundoApellido);
-		
-		textFieldSApelAutor = new JTextField();
-		textFieldSApelAutor.setColumns(10);
-		textFieldSApelAutor.setBounds(584, 385, 146, 26);
-		panel.add(textFieldSApelAutor);
-		
+				
 	}
 
 	
